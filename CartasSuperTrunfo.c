456058912d2f9810d550656552505cd8a91cab6d
tarc071 <tarcisiomota@live.com>
2025-06-13 10:22:14 +0000

    INSERI MAIS VARIÁVEIS

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,23 +12,42 @@ int main() {
     printf("BEM-VINDO AO SUPER TRUNFO ESTADOS\n");
 
     printf("INSIRA NOME DO ESTADO 1:\n");
-    scanf("%10s", Estado1);
+    scanf("%49s", Estado1);
     printf("INSIRA NOME DO ESTADO 2:\n");
-    scanf("%10s", Estado2);
+    scanf("%49s", Estado2);
 
     printf("INSIRA CÓDIGO DA CARTA 1:\n");
-    scanf("%s\n", Código1);
-    printf("INSIRA CÓDIGO DA CARTA 2:\n");
-    scanf("%s\n", Código2);
+    scanf("%s", Código1);
+    printf("INSIRA CÓDIGO DA CARTA 2: \n");
+    scanf("%s", Código2);
+
+    printf("INSIRA NOME DA CIDADE 1:\n");
+    scanf("%49s", Cidade1);
+    printf("INSIRA CÓDIGO DA CIDADE 2:\n");
+    scanf("%49s", Cidade2);
+
+    printf("INSIRA POPULAÇÃO 1:\n");
+    scanf("%d", População1);
+    printf("INSIRA POPULAÇÃO 2:\n");
+    scanf("%d", População2);
+
+    printf("INSIRA ÁREA ESTADO 1:\n");
+    scanf("%f", Área1);
+    printf("INSIRA ÁREA ESTADO 2:\n");
+    scanf("%f", Área2);
 
     printf("ESTADO 1\n");
     printf("%s\n", Estado1);
     printf("%s\n", Código1);
+    printf("%s", Cidade1);
+    printf("%f", Área1);
+
 
     printf("ESTADO 2\n");
     printf("%s\n", Estado2);
     printf("%s\n", Código2);
-
+    printf("%s", Cidade2);
+    printf("%f", Área2);
 
 
     return 0;
