7b03afff79665a3e9138d225a253831b541aebdd
tarc071 <tarcisiomota@live.com>
2025-06-13 10:55:13 +0000

    CORRIGI ERROS DE VARIÁVEIS

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,12 @@
 int main() {
    
     char Estado1[50], Estado2[50];
-    char Código1[50], Código2[50];
+    char Codigo1[50], Codigo2[50];
     char Cidade1[50], Cidade2[50];
-    int População1, População2;
-    float Área1, Área2;
+    int Populacao1, Populacao2;
+    float Area1, Area2;
           
-    printf("BEM-VINDO AO SUPER TRUNFO ESTADOS\n");
+    printf("BEM-VINDo AO SUPER TRUNFO ESTADOS\n");
 
     printf("INSIRA NOME DO ESTADO 1:\n");
     scanf("%49s", Estado1);
@@ -17,9 +17,9 @@ int main() {
     scanf("%49s", Estado2);
 
     printf("INSIRA CÓDIGO DA CARTA 1:\n");
-    scanf("%s", Código1);
+    scanf("%s", Codigo1);
     printf("INSIRA CÓDIGO DA CARTA 2: \n");
-    scanf("%s", Código2);
+    scanf("%s", Codigo2);
 
     printf("INSIRA NOME DA CIDADE 1:\n");
     scanf("%49s", Cidade1);
@@ -27,28 +27,29 @@ int main() {
     scanf("%49s", Cidade2);
 
     printf("INSIRA POPULAÇÃO 1:\n");
-    scanf("%d", População1);
+    scanf("%d", &Populacao1);
     printf("INSIRA POPULAÇÃO 2:\n");
-    scanf("%d", População2);
+    scanf("%d", &Populacao2);
 
     printf("INSIRA ÁREA ESTADO 1:\n");
-    scanf("%f", Área1);
+    scanf("%f", &Area1);
     printf("INSIRA ÁREA ESTADO 2:\n");
-    scanf("%f", Área2);
+    scanf("%f", &Area2);
 
     printf("ESTADO 1\n");
     printf("%s\n", Estado1);
-    printf("%s\n", Código1);
-    printf("%s", Cidade1);
-    printf("%f", Área1);
+    printf("%s\n", Codigo1);
+    printf("%s\n", Cidade1);
+    printf("%f\n", Area1);
 
 
     printf("ESTADO 2\n");
     printf("%s\n", Estado2);
-    printf("%s\n", Código2);
-    printf("%s", Cidade2);
-    printf("%f", Área2);
+    printf("%s\n", Codigo2);
+    printf("%s\n", Cidade2);
+    printf("%f\n", Area2);
 
+    printf("OBRIGADO POR JOGAR!");
 
     return 0;
 }
